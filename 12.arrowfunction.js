853367ec1f4d8e2cb56a223bc091d3cdda6ec9fc
Rajendra Kumar Mohapatra <rajendrakumarmohapatra0@gmail.com>
2023-04-02 11:05:55 +0530

    arrow function

---
+++ 12.arrowfunction.js
@@ -0,0 +1,12 @@
+//Arrow function
+//Arrow function with explicit return
+const sum =(a,b)=>{
+    return a+b
+}
+console.log(sum(10,10))
+
+// arrow function with implicit return
+// for implicit return remove the curley braces
+const sumimplicit =(a,b)=> a+b
+
+console.log(sumimplicit(20,30))
