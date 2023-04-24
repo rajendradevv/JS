5e6c9870ad83d4aa98e7cfc8d0731bc05ed777da
Rajendra Kumar Mohapatra <rajendrakumarmohapatra0@gmail.com>
2023-04-24 17:14:07 +0530

    this is function

---
+++ 13.exercise.js
@@ -0,0 +1,29 @@
+function add(a,b){
+    //Subtract function here
+      return a+b
+  }
+  
+  function sub(a,b){
+    //Subtract function here
+    return a-b
+  }
+  
+  function div(a,b){
+    //Divide function here
+    return a/b
+  }
+  
+  function mul(a,b){
+    //Multiply function here
+    return a*b
+  }
+  
+  console.log('hello from the SUM exercise')
+  /* 
+    TODO: create a function that console logs the result of any of the above operations.
+  */
+    console.log(add(10,2))
+    console.log(sub(10,2))
+    console.log(div(10,2))
+    console.log(mul(10,2))
+
